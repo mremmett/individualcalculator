4091f6e89cd263956d496844510e2c2f69ff3e16
mremmett <mrr36@njit.edu>
2019-10-31 18:48:32 -0400

    Feature: Subtraction + Subtraction Test

--- src/Calculator.py
+++ src/Calculator.py
@@ -3,6 +3,11 @@ def addition(a, b):
     return c
 
 
+def subtraction(a, b):
+    c = a - b
+    return c
+
+
 class Calculator:
     result = 0
 
@@ -11,4 +16,8 @@ class Calculator:
 
     def add(self, a, b):
         self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = subtraction(a, b)
         return self.result
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -18,6 +18,10 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.calculator.add(2, 2), 4)
         self.assertEqual(self.calculator.result, 4)
 
+    def test_subtract_method_calculator(self):
+        self.assertEqual(self.calculator.subtract(4, 2), 2)
+        self.assertEqual(self.calculator.result, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
