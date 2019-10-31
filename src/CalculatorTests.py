62c8f092fd37d4f3687e7bfd882c8a559ba61030
mremmett <mrr36@njit.edu>
2019-10-31 18:43:42 -0400

    Feature: Addition + Addition Test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,13 +1,22 @@
 import unittest
-
 from Calculator import Calculator
 
 
 class MyTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.calculator = Calculator()
+
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
+
+    def test_result_property_calculator(self):
+        self.assertEqual(self.calculator.result, 0)
+
+    def test_add_method_calculator(self):
+        self.assertEqual(self.calculator.add(2, 2), 4)
+        self.assertEqual(self.calculator.result, 4)
 
 
 if __name__ == '__main__':
