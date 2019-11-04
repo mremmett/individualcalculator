03974af341934f084ac82846e9dc1f286255695d
mremmett <mrr36@njit.edu>
2019-11-04 17:44:17 -0500

    Feature: Added subtraction test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -22,12 +22,12 @@ class MyTestCase(unittest.TestCase):
             self.assertEqual(self.calculator.add(float(row['Value 1']), float(row['Value 2'])), result)
             self.assertEqual(self.calculator.result, result)
 
-    #def test_subtract_method_calculator(self):
-        #test_data = CsvReader('src/UnitTestSubtraction.csv').data
-        #for row in test_data:
-            #result = float(row['Result'])
-            #self.assertEqual(self.calculator.subtract(float(row['Value 1']), float(row['Value 2'])), result)
-            #self.assertEqual(self.calculator.result, result)
+    def test_subtract_method_calculator(self):
+        test_data = CsvReader('src/UnitTestSubtraction.csv').data
+        for row in test_data:
+            result = float(row['Result'])
+            self.assertEqual(self.calculator.subtract(float(row['Value 2']), float(row['Value 1'])), result)
+            self.assertEqual(self.calculator.result, result)
 
     def test_multiply_method_calculator(self):
         test_data = CsvReader('src/UnitTestMultiplication.csv').data
